8d8457e60491befc6d79c209d61eb9375e9975a4
brendanchandev <97557517+brendanchandev@users.noreply.github.com>
2023-06-04 17:27:04 -0300

    Update serial_data_to_csv.py

--- pi/serial_data_to_csv.py
+++ pi/serial_data_to_csv.py
@@ -1,37 +1,59 @@
+import os
 import serial
 import json
 import pandas as pd
-from collections import defaultdict
+import signal
+import sys
+from collections import deque
+import time
 
-# replace '/dev/ttyACM0' with your Arduino's port
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-data = defaultdict(list)
+data = {"temperature": deque(maxlen=20), "ppm": deque(maxlen=20), "accel_data": deque(maxlen=20)}
 
-try:
-    while True:
-        try:
-            # Read the line from serial
-            line = ser.readline().decode('utf-8').strip()
+def cleanup(signum, frame):
+    # This function will be called when the script receives a SIGTERM signal
+    print("Received SIGTERM, performing cleanup and exiting...")
+    # Perform any necessary cleanup here
+    sys.exit(0)
 
-            # Parse the JSON data
-            msg = json.loads(line)
+signal.signal(signal.SIGTERM, cleanup)
 
-            # Append the data to the appropriate list
-            data['temperature'].append(msg['temperature'])
-            data['ppm'].append(msg['ppm'])
+first_read = True
 
-            # Append the accel data
-            for i in range(8):  # as we have 8 accelerometer data points (4 from flex sensors and 4 from ads1115)
-                data['accel_data'].append(msg['accel_data'][i])
+while True:
+    try:
+        if ser.inWaiting():
+            if first_read:
+                first_read = False
+                continue
+            
+            jsonData = ser.readline()
+            jsonData = jsonData.decode('utf-8', errors='replace').strip()
+            jsonData = json.loads(jsonData)
 
-        except json.JSONDecodeError:
-            print("Received malformed JSON. Skipping...")
+            data["temperature"].append(jsonData["temperature"])
+            data["ppm"].append(jsonData["ppm"])
+            data["accel_data"].append(jsonData["accel_data"])
+            
+            timestamp = time.time()
+            
+            # Append data to csv files
+            for key in data.keys():
+                file_exists = os.path.isfile(f"../data/{key}.csv")
+                if key != 'accel_data':
+                    with open(f"../data/{key}.csv", 'a') as f:
+                        df = pd.DataFrame(list(data[key]))
+                        df.insert(0, 'timestamp', timestamp)  # Insert Unix timestamp as first column
+                        df.to_csv(f, header=not file_exists, index=False)
+                else:
+                    # For accel_data, each row in csv should correspond to one value of accel_data
+                    with open(f"../data/{key}.csv", 'a') as f:
+                        df = pd.DataFrame(pd.Series(data[key]).apply(pd.Series))
+                        df.insert(0, 'timestamp', timestamp)
+                        df.to_csv(f, mode='a', header=not file_exists, index=False)
 
-except KeyboardInterrupt:
-    print("Stopping program")
-
-# Save all data to separate CSV files
-for key in data.keys():
-    df = pd.DataFrame(data[key], columns=[key])
-    df.to_csv(f"../data/{key}.csv", index=False)
+    except json.JSONDecodeError as e:
+        print(f"JSONDecodeError: {e}")
+    except UnicodeDecodeError as e:
+        print(f"UnicodeDecodeError: {e}")
